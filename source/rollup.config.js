a260978744b839e3db763c923ec364523e1bb658
Wizard <wizardsorb@protonmail.com>
2021-12-15 21:00:21 +0100

    hashicon-tsx

--- source/rollup.config.js
+++ source/rollup.config.js
@@ -3,24 +3,27 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 
-import pkg from "./package.json";
+import pkg from "../package.json";
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
-    }
-  ],
+  input: "source/index.ts",
+  // output: [
+  //   {
+  //     file: pkg.main,
+  //     format: "cjs",
+  //     exports: "named",
+  //     sourcemap: true
+  //   },
+  //   {
+  //     file: pkg.module,
+  //     format: "es",
+  //     exports: "named",
+  //     sourcemap: true
+  //   }
+  // ],
+  output: {
+    dir: './'
+  },
   plugins: [
     external(),
     resolve(),
